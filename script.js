2bf5e821904426d0bc49ca5e528b250d8fe8ee17
Brian Wong <brianw7218@gmail.com>
2022-08-12 21:41:03 -0700

    Adjust code so that the given ids for each grid cell is not zero indexed

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const ROWS = 16;
 
 const createGrids = () => {
     const gridContainer = document.querySelector(".grid-container");
-    for (let i = 0; i < COLUMNS; i++) {
-        for (let j = 0; j < ROWS; j++) {
+    for (let i = 1; i <= COLUMNS; i++) {
+        for (let j = 1; j <= ROWS; j++) {
             let grid = document.createElement("div");
             grid.classList.add("grid");
             grid.setAttribute("id", `grid-${i}-${j}`);
