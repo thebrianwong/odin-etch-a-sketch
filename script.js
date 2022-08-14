8a9a9c4b0ff7e931fc87a0789982736d545959f2
Brian Wong <brianw7218@gmail.com>
2022-08-13 21:02:35 -0700

    Create starting functionality for changing grid based on user input

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-let columns = 16;
-let rows = 16;
+let gridNumber = 16;
 
 const createGrids = () => {
     const gridContainer = document.querySelector(".grid-container");
-    for (let i = 1; i <= columns; i++) {
-        for (let j = 1; j <= rows; j++) {
+    for (let i = 1; i <= gridNumber; i++) {
+        for (let j = 1; j <= gridNumber; j++) {
             let grid = document.createElement("div");
             grid.classList.add("grid");
             grid.setAttribute("id", `grid-${i}-${j}`);
@@ -20,4 +19,21 @@ const hoverGrid = (element) => {
     })
 }
 
+const gridButton = () => {
+    let button = document.querySelector("button");
+    button.addEventListener("click", () => {
+        askForGrids();
+        createGrids();
+    })
+}
+
+const askForGrids = () => {
+    let newGridNumber = 0;
+    do {
+        newGridNumber = prompt("How many grids per side do you want? From 1 to 100.");
+    } while (newGridNumber < 1 || newGridNumber > 100)
+    gridNumber = newGridNumber;
+}
+
 createGrids();
+gridButton();
